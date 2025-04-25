1e8a1f22313ff228cd62801a2b5a6b84e9d742b4
Jimmy Ostler <jtostler1@gmail.com>
2025-04-24 22:54:56 -0700

    add reader function

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,4 +1,29 @@
+use std::{fs::File, io::BufReader};
+
 use extendr_api::prelude::*;
+use vcd::Parser;
+
+#[derive(Debug, IntoDataFrameRow)]
+struct Empty {
+    x: i32
+}
+
+/// Read a VCD file to a DataFrame
+/// @export
+#[extendr]
+fn vcd_reader(file: String) -> Result<Dataframe<Empty>> {
+    let mut reader =
+        Parser::new(BufReader::new(File::open(&file).map_err(|_| {
+            Error::Other(format!("Could not open file {}", file))
+        })?));
+
+    let header = reader
+        .parse_header()
+        .map_err(|_| Error::Other(format!("Could not parse VCD header for file {}", file)))?;
+
+    let ret_test = vec![Empty { x: 31 }];
+    Ok(ret_test.into_dataframe()?)
+}
 
 /// Return string `"Hello world!"` to R.
 /// @export
@@ -13,4 +38,5 @@ fn hello_world() -> &'static str {
 extendr_module! {
     mod vcdreader;
     fn hello_world;
+    fn vcd_reader;
 }
